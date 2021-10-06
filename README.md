3495384d3c5788fff6a9b4ec10dfbba3df35e27d
dndkwk <dndkwk@naver.com>
2021-10-07 00:04:25 +0900

    auth 리덕스 모듈에서 API 사용

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ React를 이용해 토이프로젝트 진행
 <br>&:hover의 의미: 마우스 올려져 있을 때
 <br>...props 의미: 객체의 모든 값을 복사
 <br>redux: 계층적인 컴포넌트의 상태들을 업데이트하려면 많이 거쳐가므로 글로벌한 저장소를 만들어서 거기에서 관리
-<br> Provider와 immer 공부하기
+<br> immer: 불변성을 유지할때 복잡한 코드가 요구되는데 immer가 간단하게 불변성을 유지한 객체를 생성
+<br> react-redux의 Provider: 리듀서 store를 손쉽게 연동할 수 있도록 도와주는 컴포넌트
+<br> redux-saga,middleware 알아보기
--- front_toy_project/src/modules/index.js
+++ front_toy_project/src/modules/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
+import { all } from 'redux-saga/effects';
 import loading from './loading';
-import auth from './auth';
+import auth, { authSaga } from './auth';
 
 const rootReducer = combineReducers({ auth, loading });
+
+export function* rootSaga() {
+  yield all([authSaga()]);
+}
 
 export default rootReducer;
