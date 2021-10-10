ed6bb6fe4357eb047a8da14f32ac531d5d471c83
dndkwk <dndkwk@naver.com>
2021-10-10 14:19:55 +0900

    로그인시 에러처리

--- front_toy_project/src/containers/auth/LoginForm.js
+++ front_toy_project/src/containers/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
@@ -7,6 +7,7 @@ import { check } from '../../modules/user';
 
 const LoginForm = ({ history }) => {
   const dispatch = useDispatch();
+  const [error, setError] = useState(null);
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.login,
     auth: auth.auth,
@@ -39,6 +40,7 @@ const LoginForm = ({ history }) => {
     if (authError) {
       console.log('오류 발생');
       console.log('authError');
+      setError('로그인 실패');
       return;
     }
     if (auth) {
@@ -59,6 +61,7 @@ const LoginForm = ({ history }) => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
