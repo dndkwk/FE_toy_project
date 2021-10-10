616fd81dabecddf675305971e787b29bfe98ea83
dndkwk <dndkwk@naver.com>
2021-10-10 14:31:31 +0900

    회원 인증 에러 처리

--- README.md
+++ README.md
@@ -6,10 +6,11 @@ React를 이용해 토이프로젝트 진행, 백엔드는 나중에 만들 것
 <br>&:hover의 의미: 마우스 올려져 있을 때
 <br>...props 의미: 객체의 모든 값을 복사
 <br>redux: 계층적인 컴포넌트의 상태들을 업데이트하려면 많이 거쳐가므로 글로벌한 저장소를 만들어서 거기에서 관리
-<br> immer: 불변성을 유지할때 복잡한 코드가 요구되는데 immer가 간단하게 불변성을 유지한 객체를 생성
+<br> immer: 불변성을 유지할때 복잡한 코드가 요구되는데 immer가 간단하게 불변성을 유지한 객체를 생성(produce 사용)
 <br> react-redux의 Provider: 리듀서 store를 손쉽게 연동할 수 있도록 도와주는 컴포넌트
 <br> useSelector: 리덕스의 state를 조회 가능
 <br> useEffect: 리액트 컴포넌트가 랜더링될 때마다 특정 작업을 수행하도록 만듬
 <br> useDispatch: 생성한 action을 발생시킬 수 있다.
 <br> withRouter: 라우터에 의해서 호출된 컴포넌트가 아니여도 match,location,history 객체에 접근할 수 있도록 해줌, history.push('/') 는 홈으로 돌아가는 뜻
-<br> redux-saga,middleware 알아보기
+<br> yield,includes(''),redux-saga,middleware 알아보기
+<br> authError.response.status === 409 의미
--- front_toy_project/src/containers/auth/RegisterForm.js
+++ front_toy_project/src/containers/auth/RegisterForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
@@ -7,6 +7,7 @@ import { withRouter } from 'react-router';
 
 const RegisterForm = ({ history }) => {
   const dispatch = useDispatch();
+  const [error, setError] = useState(null);
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
     auth: auth.auth,
@@ -34,7 +35,18 @@ const RegisterForm = ({ history }) => {
   const onSubmit = (e) => {
     e.preventDefault();
     const { username, password, passwordConfirm } = form;
+    if ([username, password, passwordConfirm].includes('')) {
+      setError('빈 칸을 모두 입력하세요.');
+      return;
+    }
     if (password !== passwordConfirm) {
+      setError('비밀번호가 일치하지 않습니다.');
+
+      //다시 비번/비번확인 자리 공백으로 만들기
+      dispatch(changeField({ form: 'register', key: 'password', value: '' }));
+      dispatch(
+        changeField({ form: 'register', key: 'passwordConfirm', value: '' }),
+      );
       return;
     }
     dispatch(register({ username, password }));
@@ -46,10 +58,14 @@ const RegisterForm = ({ history }) => {
 
   useEffect(() => {
     if (authError) {
-      console.log('오류발생');
-      console.log(authError);
+      if (authError.response.status === 409) {
+        setError('이미 존재하는 계정입니다.');
+        return;
+      }
+      setError('회원가입 실패');
       return;
     }
+
     if (auth) {
       console.log('회원가입 성공');
       console.log(auth);
@@ -70,6 +86,7 @@ const RegisterForm = ({ history }) => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
