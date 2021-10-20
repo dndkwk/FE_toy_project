6dcc8209f90146b2965d78ef6ba7a84fb152f200
dndkwk <dndkwk@naver.com>
2021-10-20 23:34:55 +0900

    글쓰기 API 연동

--- front_toy_project/src/modules/index.js
+++ front_toy_project/src/modules/index.js
@@ -3,12 +3,12 @@ import { all } from 'redux-saga/effects';
 import loading from './loading';
 import auth, { authSaga } from './auth';
 import user, { userSaga } from './user';
-import write from './write';
+import write, { writeSaga } from './write';
 
 const rootReducer = combineReducers({ auth, loading, user, write });
 
 export function* rootSaga() {
-  yield all([authSaga(), userSaga()]);
+  yield all([authSaga(), userSaga(), writeSaga]);
 }
 
 export default rootReducer;
